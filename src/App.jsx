1a583f05dbb34d2d2369342e936648fb5afa3e76
Wagner Santos <114152919+WagSnt@users.noreply.github.com>
2023-11-13 23:49:11 +0000

    Criação de tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
     <>
       <div className="card">
         <h2 className='nameTitle'>Rick Sanchez</h2>
+        <div className='tags'>
+          <div className='tag'>Status: Vivo</div>
+          <div className='tag'>Espécie: Humana</div>
+          <div className='tag'>Origem: Terra C-137</div>
+        </div>
+        
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
       </div>
       
