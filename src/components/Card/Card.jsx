14ce8f1255b361bd9494a1cdd8f2b1dc5553e356
Wagner Santos <114152919+WagSnt@users.noreply.github.com>
2023-11-14 22:53:37 +0000

    Correção desafio 2

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,13 @@
+import Tag from "../Tag/Tag"
+
 export default function Card(props) {
     const item = props.item
     return <div className="card">
     <h2 className='nameTitle'>{item.name}</h2>
     <div className='tags'>
-      <div className='tag'>Status: Vivo</div>
-      <div className='tag'>Espécie: Humana</div>
-      <div className='tag'>Origem: Terra C-137</div>
+      <Tag text=" Status: Vivo"/>
+      <Tag text="Espécie Humana"/>
+      <Tag text="Origem: Terra C-137"/>
     </div>
     <img src={item.imageUrl} alt="" />
   </div>    
